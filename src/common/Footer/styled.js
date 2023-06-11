d62bd48867cb78de771cfbfa79485cb4497ca9fa
Alicja Jurczak <ala.jurczak@gmail.com>
2023-06-11 15:30:30 +0200

    Footer style added

---
+++ src/common/Footer/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import heart from "./serce.png";
+
+export const Heart = styled.div`
+  background-image: url(${heart});
+  background-size: cover;
+  width: 25px;
+  height: 25px;
+  margin: 25px auto;
+`;
